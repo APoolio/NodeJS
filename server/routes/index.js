69842bb41cd7b1fe250c12d46cf07bcf4bfb85d9
APoolio <andrewspooleap@gmail.com>
2021-02-04 23:41:56 -0600

    Updating package.json and several routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
 //Head honcho routes file
 
 const routes = require('express').Router();
-const models = require('./models');
 var stocks = require('./stocks');
 
 routes.get('/', function(req, res){
   res.render('main');
 });
 
-routes.use('/models', models);
 routes.use('/stocks', stocks);
 
 module.exports = routes;
